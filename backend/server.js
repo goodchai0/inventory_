57c041980ef53b36592b98b9bfba10381e757160
Amit <www.amitkewot@gmail.com>
2023-04-20 03:07:17 +0530

    wip

--- backend/server.js
+++ backend/server.js
@@ -15,11 +15,11 @@ const path = require("path");
 const app = express();
 
 // Middlewares
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(cors());
 
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use(express.static(path.join(__dirname, "./frontend/build")));
